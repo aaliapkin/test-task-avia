431503cbb283050a6911be8bd40e0f48bdc391cc
aaliapkin <aaliapkin@gmail.com>
2020-11-10 14:09:35 +0300

    Updates

---
+++ precache-manifest.5374f57230f614b0add7534a3738405b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68eaaf954b2cd9d76558868e5eda936d",
+    "url": "/test-task-avia/index.html"
+  },
+  {
+    "revision": "2257a4866e4150aba4c4",
+    "url": "/test-task-avia/static/css/main.3f9ffc6b.chunk.css"
+  },
+  {
+    "revision": "f7591286a8b7cf5a4d8b",
+    "url": "/test-task-avia/static/js/2.79556a36.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/test-task-avia/static/js/2.79556a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2257a4866e4150aba4c4",
+    "url": "/test-task-avia/static/js/main.a0256734.chunk.js"
+  },
+  {
+    "revision": "866821d50bde00a07df2",
+    "url": "/test-task-avia/static/js/runtime-main.86126b66.js"
+  }
+]);
